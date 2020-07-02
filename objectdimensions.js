257a95957be3985f4cb3d1360c08f7e3f698bada
juergenweb <juergenweb@users.noreply.github.com>
2020-07-02 12:12:37 +0200

    Update objectdimensions.js

--- objectdimensions.js
+++ objectdimensions.js
@@ -7,17 +7,7 @@ function restrict(tis, digits) {
   if (isNaN(num)) {
     num.val(0);
   }
-/*
-  var pattern = new RegExp(/^[0-9.,]+$/); // regex to allow only numbers, dot and comma
-  var res = pattern.test(num);
 
-  if(res == false){
-
-    num = num.slice(0, -1);
-
-  }
-
-  num = tis.value;*/
   if ((num.indexOf(".") > -1)  && (num.split('.')[1].length > digits)) {
     var newValue = num.substring(0, num.length - 1);
     tis.value = newValue;
