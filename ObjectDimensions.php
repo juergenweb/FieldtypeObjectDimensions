f69f143c70da99c67430c5b11f40574f35264761
juergenweb <juergenweb@users.noreply.github.com>
2020-07-02 12:13:40 +0200

    Update ObjectDimensions.php

--- ObjectDimensions.php
+++ ObjectDimensions.php
@@ -7,42 +7,73 @@ namespace ProcessWire;
  */
 class ObjectDimensions extends WireData {
 
-    protected $page;
-
-
     public function __construct() {
+      parent::__construct();
+
       try {
         $this->set('width', null);
         $this->set('height', null);
         $this->set('depth', null);
+        $this->set('volume', null);
+        $this->set('floorspace', null);
+        $this->set('unit', null);
       }
       catch (WireException $e) {
       }
     }
 
     public function set($key, $value) {
-
-        if($key == 'width' || $key == 'height' || $key == 'depth') {
-            // if value isn't numeric, don't change the value if already
-            // one set, else set it to 0 and throw an exception so it can be seen on API usage
-            if(!is_numeric($value) && !is_null($value)) {
-                $value = $this->$key ? $this->$key : 0;
-                throw new WireException("Dimension Object only accepts numbers (float or integer) values");
-            }
-        }
         return parent::set($key, $value);
     }
 
     public function get($key) {
         return parent::get($key);
+    }
 
+    /**
+    * Method to render all dimensions in a string like 25cm * 10cm * 25cm
+    * @param string $multiplicator (the sign between the values)
+    * @param bool $addUnit (adds fe cm after the value) true|false
+    * @return string
+    */
+    public function renderDimensions(string $multiplicator = ' * ', bool $addUnit = true): string
+    {
+      $unit = $addUnit ? $this->unit : '';
+      $width = $this->width ? $this->width.$unit : '';
+      $height = $this->height ? $this->height.$unit : '';
+      $depth = $this->depth ? $this->depth.$unit : '';
+      $dimensions = array_filter([$width, $height, $depth]);
+      return (count($dimensions)) ? implode($multiplicator, $dimensions) : '';
     }
 
 
-    // for echo $field; directly
+    /**
+    * Method to render the volume fe 25cm³
+    * @param bool $addUnit (adds fe cm³ after the value) true|false
+    * @return string
+    */
+    public function renderVolume(bool $addUnit = true): string
+    {
+      $unit = $addUnit ? $this->unit.'<sup>3</sup>' : '';
+      return ($this->volume > 0) ? $this->volume.$unit : '';
+    }
+
+
+    /**
+    * Method to render the spacefloor fe 25cm²
+    * @param bool $addUnit (adds fe cm² after the value) true|false
+    * @return string
+    */
+    public function renderFloorSpace(bool $addUnit = true): string
+    {
+      $unit = $addUnit ? $this->unit.'<sup>2</sup>' : '';
+      return ($this->floorspace > 0) ? $this->floorspace.$unit : '';
+    }
+
+
     public function __toString()
     {
-        return $this->width.'*'.$this->height.'*'.$this->depth;
+      return $this->renderDimensions();
     }
 
 }
